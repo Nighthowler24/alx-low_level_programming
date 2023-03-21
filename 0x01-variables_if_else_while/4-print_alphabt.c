1c3adb8fdc739b8fcaba21b1a441ed0e7ddbe475
Nighthowler24 <ayomideomotosho27@gmail.com>
2023-03-20 21:16:02 -0700

    alltasks

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main(void){
-char ch;
-for(ch='a'; ch<='z'; ch++){
-if (ch !='e'; && ch !='q'){
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	if (ch != 'e' && ch != 'q')
 	putchar(ch);
-}
-}
-putchar('\n');
-return(0);
+
+	putchar('\n');
+	return	(0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main(void){
-int n;
-for (n=0; n<=9; n++){
-	putchar(n);
-}
-putchar("\n");
-return(0);
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
 }
