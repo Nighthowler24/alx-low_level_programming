9c2b0243581678de787ff6635849dc1cbaf84bc2
Nighthowler24 <ayomideomotosho27@gmail.com>
2023-03-22 00:45:10 -0700

    alltasks

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
  * _islower - returns 1 for a lowercase alphabet else return 0
+ *
+ * @c: parameter to outputted
+ *
  * Return: 1 if true else false
  */
 
 int _islower(int c)
 {
-	int r;
-
 	if (c <= 112 && c >= 97)
 		return (1);
 	else
