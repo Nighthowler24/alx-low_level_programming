54f21f36931da61bdc8884b8bf5d69d5ee8abb5f
Nighthowler24 <ayomideomotosho27@gmail.com>
2023-03-22 18:22:58 -0700

    alltasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @d: value to determine last digit
+ * Return: the last digit of the value given,any number.
+ */
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -(k);
+	_putchar('0' + k);
+	return (k);
+}
+
