0f82d2b7233a945a877f667e55bec40ef77268da
Nighthowler24 <ayomideomotosho27@gmail.com>
2023-03-22 20:57:55 -0700

    alltasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two integers together
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: the sum of two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+    times_table();
+    return (0);
+}
