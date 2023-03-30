6190c472acefcbf8a3c0f814d74c6650d1a7b73a
Nighthowler24 <ayomideomotosho27@gmail.com>
2023-03-30 06:48:08 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper- converts string to uppercase
+ * @s: string
+ * Return: returns s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
